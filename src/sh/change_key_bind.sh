dc8865fc8703bda12e1265b9b7d67efe7742c272
shoken <syokenz@gmail.com>
2017-07-19 08:53:28 +0900

    change file name

--- src/sh/change_key_bind.sh
+++
@@ -1,3 +0,0 @@
-# change right shift to delete
-# change right option to Grave Accent and Tilde
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":  0x7000000e5,"HIDKeyboardModifierMappingDst":0x70000002a},{"HIDKeyboardModifierMappingSrc":  0x7000000e6,"HIDKeyboardModifierMappingDst":0x700000035}]}'
---
+++ src/sh/key_remapper.sh
@@ -0,0 +1,3 @@
+# remap right shift to delete
+# remap right option to Grave Accent and Tilde
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":  0x7000000e5,"HIDKeyboardModifierMappingDst":0x70000002a},{"HIDKeyboardModifierMappingSrc":  0x7000000e6,"HIDKeyboardModifierMappingDst":0x700000035}]}'
